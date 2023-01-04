a36c4277b8699b7e3f7995c4af92c316fdf11b30
IvanPrz5 <maperLBR@gmail.com>
2023-01-04 15:23:59 -0600

    commitCInverso

--- algo.js
+++ algo.js
@@ -21,3 +21,57 @@
     this.isrData = this.isrData.toFixed(2);
     this.netoData = this.ingresoData - this.isrData;
     this.netoData = this.netoData.toFixed(2); */
+
+
+/* calculaIsr() {
+  let parse = parseFloat(this.netoData);
+  let number = parseFloat(this.netoData) * 2;
+  for (let i = parse; i <= number; i += 0.01) {
+    //console.log(i);
+    let año = {
+      2015: arrayClass2017,
+      2016: arrayClass2017,
+      2017: arrayClass2017,
+      2018: arrayClass2020,
+      2019: arrayClass2020,
+      2020: arrayClass2020,
+      2021: arrayClass,
+      2022: arrayClass,
+    };
+
+    let selecAño = año[this.añoData];
+    //let selec = "Mensual";
+    let periodo = {
+      Anual: selecAño.arrayIsrAnual,
+      Mensual: selecAño.arrayIsrMen,
+      Quincenal: selecAño.arrayIsrQuin,
+      Semanal: selecAño.arrayIsrSem,
+      Diario: selecAño.arrayIsrDiario,
+    };
+
+    let selecPeriodo = periodo[this.periodoData];
+    selecPeriodo.map((isrObj) => {
+      //let i = this.netoData;
+      if (isrObj.limInfe < i && isrObj.limSup > i) {
+        let limiteI = isrObj.limInfe;
+        //this.limInfe = limiteI;
+        let tasa = isrObj.Porcentaje;
+        let cuotaFija = isrObj.Cuota;
+        let base = i - limiteI;
+        let impuestoMarginal = base * tasa;
+        let isrData = impuestoMarginal + cuotaFija;
+        this.brutoData = i - isrData;
+        //let parse2 = parseFloat(temporal);
+        this.brutoData = this.brutoData.toFixed(2);
+        console.log(i.toFixed(2));
+        console.log(isrData.toFixed(2));
+        console.log(this.brutoData);
+        if (this.netoData === this.brutoData) {
+          this.brutoData2 = i.toFixed(2);
+        }
+        return isrObj;
+      }
+    });
+  }
+},
+ */
--- src/components/FormR.vue
+++ src/components/FormR.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
-    <v-combobox v-model="añoData" :items="años" label="Año" outlined dense></v-combobox>
-    <v-combobox v-model="periodoData" :items="periodo" label="Periodo" outlined dense></v-combobox>
-    <v-text-field v-model="netoData" label="Neto" aria-required="true" outlined dense></v-text-field>
+    <v-combobox
+      v-model="añoData"
+      :items="años"
+      label="Año"
+      outlined
+      dense
+    ></v-combobox>
+    <v-combobox
+      v-model="periodoData"
+      :items="periodo"
+      label="Periodo"
+      outlined
+      dense
+    ></v-combobox>
+    <v-text-field
+      v-model="netoData"
+      label="Neto"
+      aria-required="true"
+      outlined
+      dense
+    ></v-text-field>
     <div class="btn-container">
       <v-btn depressed color="primary" @click="calculaIsr()"> Calcular </v-btn>
     </div>
-    <v-text-field v-model="brutoData" label="Bruto" outlined dense readonly></v-text-field>
+    <v-text-field
+      v-model="brutoData"
+      label="Bruto"
+      outlined
+      dense
+      readonly
+    ></v-text-field>
+    <v-text-field
+      v-model="brutoData2"
+      label="Bruto"
+      outlined
+      dense
+      readonly
+    ></v-text-field>
     <!-- <v-text-field v-model="datos.porcentaje" label="Ingreso" aria-required="true" outlined dense></v-text-field>
     <v-text-field v-model="datos.cuota" label="Ingreso" aria-required="true" outlined dense></v-text-field> -->
   </div>
@@ -14,13 +45,10 @@
 
 <script>
 import axios from "axios";
-import { arrayClass } from "../src/arrayClass";
-import { arrayClass2020 } from "../src/arrayClass2020";
-import { arrayClass2017 } from "../src/arrayClass2017";
 
 export default {
   name: "FormR",
-  props: ['datos'],
+  props: ["datos"],
   data() {
     return {
       añoData: "",
@@ -39,106 +67,47 @@ export default {
       periodo: ["Anual", "Mensual", "Quincenal", "Semanal", "Diario"],
       netoData: "",
       brutoData: "",
+      brutoData2: "",
       result: null,
-    }
+    };
   },
   methods: {
-    calculaBruto() {
-      let año = {
-        2015: arrayClass2017,
-        2016: arrayClass2017,
-        2017: arrayClass2017,
-        2018: arrayClass2020,
-        2019: arrayClass2020,
-        2020: arrayClass2020,
-        2021: arrayClass,
-        2022: arrayClass,
-      };
-
-      let selecAño = año[this.añoData];
-      //let selec = "Mensual";
-      let periodo = {
-        Anual: selecAño.arrayIsrAnual,
-        Mensual: selecAño.arrayIsrMen,
-        Quincenal: selecAño.arrayIsrQuin,
-        Semanal: selecAño.arrayIsrSem,
-        Diario: selecAño.arrayIsrDiario,
-      };
-
-      let selecPeriodo = periodo[this.periodoData];
-      selecPeriodo.map((isrObj) => {
-        if (
-          isrObj.limInfe < this.netoData &&
-          isrObj.limSup > this.netoData
-        ) {
-          let limiteI = isrObj.limInfe;
-          this.limInfe = limiteI;
-          let tasa = isrObj.Porcentaje;
-          let cuotaFija = isrObj.Cuota;
-          let base = this.netoData - limiteI;
-          let impuestoMarginal = base * tasa;
-          this.isrData = impuestoMarginal + cuotaFija;
-          this.isrData = this.isrData.toFixed(2);
-          this.brutoData = this.netoData - this.isrData;
-          this.brutoData = this.brutoData.toFixed(2);
-          //console.log("$ " + this.isrData.toFixed(2));
-          return isrObj;
-        }
-      });
-    },
     calculaIsr() {
-      let number = this.netoData * 2
-      for (let i = this.netoData; i < number; i++) {
-        //console.log(i);
-        let año = {
-          2015: arrayClass2017,
-          2016: arrayClass2017,
-          2017: arrayClass2017,
-          2018: arrayClass2020,
-          2019: arrayClass2020,
-          2020: arrayClass2020,
-          2021: arrayClass,
-          2022: arrayClass,
-        };
-
-        let selecAño = año[this.añoData];
-        //let selec = "Mensual";
-        let periodo = {
-          Anual: selecAño.arrayIsrAnual,
-          Mensual: selecAño.arrayIsrMen,
-          Quincenal: selecAño.arrayIsrQuin,
-          Semanal: selecAño.arrayIsrSem,
-          Diario: selecAño.arrayIsrDiario,
-        };
-
-        let selecPeriodo = periodo[this.periodoData];
-        selecPeriodo.map((isrObj) => {
-          //let i = this.netoData;
-          if (
-            isrObj.limInfe < i &&
-            isrObj.limSup > i
-          ) {
-            let limiteI = isrObj.limInfe;
-            /* this.limInfe = limiteI; */
-            let tasa = isrObj.Porcentaje;
-            let cuotaFija = isrObj.Cuota;
-            let base = i - limiteI;
-            let impuestoMarginal = base * tasa;
-            this.isrData = impuestoMarginal + cuotaFija;
-            this.isrData = this.isrData.toFixed(2);
-            let isrData = this.isrData;
-            //console.log(isrData);
-            this.brutoData = i - isrData;
-            this.brutoData = this.brutoData.toFixed(2);
-            /* console.log(this.brutoData);
-            console.log(this.isrData) */
-            let algo = (parseFloat(i) + parseFloat(isrData));
-            console.log(algo);
-            return isrObj;
+      axios
+        .get(
+          "http://localhost:8081/Isr/año/" +
+            this.añoData +
+            "/" +
+            this.periodoData
+        )
+        .then((response) => {
+          this.result = response.data;
+          for (let i = 0; i < this.result.length; i++) {
+            //console.log(response.data[i].limInf);
+            let parse = parseFloat(this.netoData);
+            let number = parseFloat(this.netoData) * 2;
+            for (let j = parse; j <= number; j += 0.01) {
+              let limInf = response.data[i].limInf;
+              let porcentaje = response.data[i].porcentaje;
+              let cuota = response.data[i].cuota;
+              if (response.data[i].limInf < j && response.data[i].limSup > j) {
+                this.limInf = limInf;
+                let base = j - limInf;
+                let impuestoMarginal = base * porcentaje;
+                let isrData = impuestoMarginal + cuota;
+                this.brutoData = j - isrData;
+                this.brutoData = this.brutoData.toFixed(2);
+                /* console.log(j.toFixed(2));
+                console.log(isrData.toFixed(2))
+                console.log(this.brutoData) */
+                if (this.netoData === this.brutoData) {
+                  this.brutoData2 = j.toFixed(2);
+                }
+              }
+            }
           }
         });
-      }
-    }
-  }
+    },
+  },
 };
 </script>
